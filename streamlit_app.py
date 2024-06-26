46ee88b394e4632df6a8de77a8d1a945c7c691b5
silviamat <103664459+silviamat@users.noreply.github.com>
2024-06-26 10:26:53 +0000

    change description and add requirement

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ import openai
 from openai import OpenAI
 from langchain.prompts import ChatPromptTemplate
 
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
-openai.api_key = os.environ['OPENAI_API_KEY']
+#from dotenv import load_dotenv, find_dotenv
+#_ = load_dotenv(find_dotenv()) # read local .env file
+#openai.api_key = os.environ['OPENAI_API_KEY']
 llm_model = "gpt-3.5-turbo"
 
 openai_api_key = 'sk-proj-UwIFmR4JTFkG2fZQkyTMT3BlbkFJ0rmL1AOupeUBeNA8oiYd'
@@ -29,20 +29,12 @@ st.title('🧑‍⚕️ Dr.Lang')
 
 with st.expander('About this chat'):
   st.markdown('**What can this chat do?**')
-  st.info('This app allow users to build a machine learning (ML) model in an end-to-end workflow. Particularly, this encompasses data upload, data pre-processing, ML model building and post-model analysis.')
-
+  st.info('Dr.Lang can help anyone understand their healthcare reports.')
   st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, go to the sidebar and 1. Select a data set and 2. Adjust the model parameters by adjusting the various slider widgets. As a result, this would initiate the ML model building process, display the model results as well as allowing users to download the generated models and accompanying data.')
-
-  st.markdown('**Under the hood**')
-  st.markdown('Data sets:')
-  st.code('''- Drug solubility data set
-  ''', language='markdown')
+  st.warning('To engage with the app, go to the sidebar and upload your medical analysis. As a result, Dr.Lang will throughly explain the results.')
   
   st.markdown('Libraries used:')
-  st.code('''- Pandas for data wrangling
-- Scikit-learn for building a machine learning model
-- Altair for chart creation
+  st.code('''- LangChain for LLM development
 - Streamlit for user interface
   ''', language='markdown')
 
@@ -57,7 +49,7 @@ with st.sidebar:
         df = pd.read_csv(uploaded_file, index_col=False)
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Please upload your heaalthcare report so I can help you"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
